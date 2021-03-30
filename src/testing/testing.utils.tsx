2b92754899683fd51790b47317fb6e907b29d6af
pchmn <pchmn.dev@gmail.com>
2021-03-31 01:47:30 +0200

    test: fix redux Provider error

--- src/testing/testing.utils.tsx
+++ src/testing/testing.utils.tsx
@@ -1,5 +1,8 @@
+import store from '@core/store/store';
 import { matchers } from '@emotion/jest';
 import { render, RenderResult } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import * as theme from './theme.json';
 
@@ -15,5 +18,9 @@ jest.mock('react-i18next', () => ({
 
 export const renderWithRouter = (ui: React.ReactElement, { route = '/' } = {}): RenderResult<any> => {
   window.history.pushState({}, 'Test page', route);
-  return render(ui, { wrapper: BrowserRouter });
+  return render(<Provider store={store}>{ui}</Provider>, { wrapper: BrowserRouter });
+};
+
+export const renderWithStore = (ui: React.ReactElement): RenderResult => {
+  return render(<Provider store={store}>{ui}</Provider>);
 };
