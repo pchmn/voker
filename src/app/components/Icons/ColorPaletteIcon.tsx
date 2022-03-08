155a292eee1b1087ecfac9a8ece278406ff55e85
pchmn <pchmn.dev@gmail.com>
2022-03-08 11:42:37 +0100

    feat: create icon components

---
+++ src/app/components/Icons/ColorPaletteIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function ColorPaletteIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
+      <g fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M12 21a9 9 0 1 1 0-18a9 8 0 0 1 9 8a4.5 4 0 0 1-4.5 4H14a2 2 0 0 0-1 3.75A1.3 1.3 0 0 1 12 21"></path>
+        <circle cx="7.5" cy="10.5" r=".5" fill="currentColor"></circle>
+        <circle cx="12" cy="7.5" r=".5" fill="currentColor"></circle>
+        <circle cx="16.5" cy="10.5" r=".5" fill="currentColor"></circle>
+      </g>
+    </svg>
+  );
+}
--- src/app/components/Icons/UserIcon.tsx
+++ src/app/components/Icons/UserIcon.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export function MoonIcon() {
+export function UserIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
       <g fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M12 3h.393a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.992z"></path>
-        <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0-2 2a2 2 0 0 0-2-2a2 2 0 0 0 2-2"></path>
-        <path d="M19 11h2m-1-1v2"></path>
+        <circle cx="12" cy="12" r="9"></circle>
+        <circle cx="12" cy="10" r="3"></circle>
+        <path d="M6.168 18.849A4 4 0 0 1 10 16h4a4 4 0 0 1 3.834 2.855"></path>
       </g>
     </svg>
   );
