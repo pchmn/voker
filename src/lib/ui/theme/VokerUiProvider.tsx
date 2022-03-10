d60d0dedd425433831fd1ee1686589b6e1e903a7
pchmn <pchmn.dev@gmail.com>
2022-03-10 10:23:36 +0100

    fix: prevent rerender by removing `ColorSchemeProvider`

--- src/lib/ui/theme/VokerUiProvider.tsx
+++ src/lib/ui/theme/VokerUiProvider.tsx
@@ -1,18 +1,15 @@
 import { ThemeSettingsContext, useThemeSettings } from '@lib/ui';
-import { ColorSchemeProvider, MantineProvider, TypographyStylesProvider } from '@mantine/core';
+import { MantineProvider, TypographyStylesProvider } from '@mantine/core';
 import React, { PropsWithChildren } from 'react';
 
 export function VokerUiProvider({ children }: PropsWithChildren<unknown>) {
   const { themeSettings, toggleColorScheme, setPrimaryColor } = useThemeSettings();
-  console.log('themeSettings', themeSettings);
 
   return (
     <ThemeSettingsContext.Provider value={{ themeSettings, toggleColorScheme, setPrimaryColor }}>
-      <ColorSchemeProvider colorScheme={themeSettings?.colorScheme || 'dark'} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider theme={themeSettings}>
-          <TypographyStylesProvider>{children}</TypographyStylesProvider>
-        </MantineProvider>
-      </ColorSchemeProvider>
+      <MantineProvider theme={themeSettings}>
+        <TypographyStylesProvider>{children}</TypographyStylesProvider>
+      </MantineProvider>
     </ThemeSettingsContext.Provider>
   );
 }
