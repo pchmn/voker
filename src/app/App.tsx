a3a440d82d78102abdfb7d6c15493a59cd5e26e3
pchmn <pchmn.dev@gmail.com>
2022-03-28 13:13:01 +0200

    feat: add routing with `react-location`

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,32 @@
 import { withAuth } from '@app/core/auth';
 import { AppLayout } from '@app/core/layout';
-import { Home } from '@app/features/Home';
+import { Home } from '@app/modules/Home';
+import { ReactLocation, Router } from '@tanstack/react-location';
+import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import './App.css';
+
+const location = new ReactLocation();
 
 function App() {
   return (
     <AppLayout>
-      <Home />
+      <Router
+        basepath="/voker"
+        location={location}
+        routes={[
+          {
+            path: '/',
+            element: (
+              <AnimatePresence>
+                <motion.div key="modal" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                  <Home />
+                </motion.div>
+              </AnimatePresence>
+            )
+          }
+        ]}
+      ></Router>
     </AppLayout>
   );
 }
--- src/app/core/auth/hoc/withAuth.tsx
+++ src/app/core/auth/hoc/withAuth.tsx
@@ -1,8 +1,67 @@
 import { DynamicLogo } from '@app/shared/components';
 import { useFirebaseAuth } from '@lib/core';
-import { AppShell, Title } from '@mantine/core';
+import { Title } from '@mantine/core';
+import { AnimatePresence, motion } from 'framer-motion';
+import { keyframes, styled } from 'goober';
 import React, { useEffect, useState } from 'react';
-import './withAuth.scss';
+
+const scale = keyframes({
+  '0%': {
+    transform: 'scale(0)'
+  },
+  '40% ': {
+    transform: 'scale(1.2)'
+  },
+  '50%': {
+    transform: 'scale(1.0)'
+  }
+});
+
+const pulse = keyframes({
+  '0%': {
+    transform: 'scale(1.0)'
+  },
+  '40% ': {
+    transform: 'scale(1.1)'
+  },
+  '50%': {
+    transform: 'scale(1.0)'
+  }
+});
+
+const translate = keyframes({
+  '0%': {
+    transform: 'translateY(-200%)',
+    opacity: 0
+  },
+  '40% ': {
+    transform: 'translateY(25%)',
+    opacity: 1
+  },
+  '50%': {
+    transform: 'translateY(0)',
+    opacity: 1
+  }
+});
+
+const Container = styled('div')(({ theme }) => ({
+  backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh'
+}));
+
+const AppLogo = styled(DynamicLogo)(() => ({
+  animation: `${scale} 1s ease-in-out forwards, ${pulse} 1s ease infinite`,
+  animationDelay: '0ms, 1.2s'
+}));
+
+const AppName = styled(Title)(() => ({
+  animation: `${translate} 1s ease-in-out forwards`,
+  marginTop: '0 !important'
+}));
 
 export function withAuth(Component: React.ElementType) {
   return function Render(): JSX.Element {
@@ -16,36 +75,25 @@ export function withAuth(Component: React.ElementType) {
       });
     }, [signIn]);
 
-    const onAnimation = (start: boolean) => console.log(`animation ${start ? 'started' : 'finished'}`);
-
     if (loading || !animationEnded) {
       return (
-        <AppShell
-          fixed
-          styles={(theme) => ({
-            main: {
-              backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center'
-            }
-          })}
-          className="withAuth"
-        >
-          <DynamicLogo size="xl" className="appLogo" />
-          <Title
-            className="appName"
-            order={1}
-            onAnimationStart={() => onAnimation(true)}
-            onAnimationEnd={() => setAnimationEnded(true)}
-          >
+        <Container>
+          <AppLogo size="xl" />
+          <AppName order={1} onAnimationEnd={() => setAnimationEnded(true)}>
             Voker
-          </Title>
-        </AppShell>
+          </AppName>
+        </Container>
       );
     }
 
-    return <Component />;
+    return (
+      <Container>
+        <AnimatePresence>
+          <motion.div key="modal" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+            <Component />
+          </motion.div>
+        </AnimatePresence>
+      </Container>
+    );
   };
 }
