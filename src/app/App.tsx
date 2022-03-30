bec0fb06f94e9d441cc5f4f4d09a186fc2f92ce7
pchmn <pchmn.dev@gmail.com>
2022-03-30 19:00:46 +0200

    feat: create `AnimatedRoute` component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import { withAuth } from '@app/core/auth';
 import { AppLayout } from '@app/core/layout';
 import { Home } from '@app/modules/Home';
+import { AnimatedRoute } from '@lib/ui';
 import { ReactLocation, Router } from '@tanstack/react-location';
-import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import './App.css';
 
@@ -14,15 +14,14 @@ function App() {
       <Router
         basepath="/voker"
         location={location}
+        key={location.current.pathname}
         routes={[
           {
             path: '/',
             element: (
-              <AnimatePresence>
-                <motion.div key="modal" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-                  <Home />
-                </motion.div>
-              </AnimatePresence>
+              <AnimatedRoute>
+                <Home />
+              </AnimatedRoute>
             )
           }
         ]}
---
+++ src/lib/ui/components/AnimatedRoute/AnimatedRoute.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from '@tanstack/react-location';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { PropsWithChildren } from 'react';
+
+const variants = {
+  initial: {
+    opacity: 0,
+    y: 8
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.2,
+      ease: [0.61, 1, 0.88, 1]
+    }
+  },
+  exit: {
+    opacity: 0,
+    y: 8,
+    transition: {
+      duration: 0.2,
+      ease: [0.61, 1, 0.88, 1]
+    }
+  }
+};
+
+export function AnimatedRoute({ children }: PropsWithChildren<unknown>) {
+  const router = useRouter();
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <motion.div key={router.state.location.key} {...variants}>
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
