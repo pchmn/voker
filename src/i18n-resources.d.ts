79221281ee1c15c016ed74dfb4a75612d4098e6e
pchmn <pchmn.dev@gmail.com>
2022-04-14 18:43:49 +0200

    conf: fix i18n typings

--- src/i18n-resources.d.ts
+++ src/i18n-resources.d.ts
@@ -1,7 +1,15 @@
 import 'react-i18next';
+import en from './config/i18n/en.json';
+import fr from './config/i18n/fr.json';
 
 declare module 'react-i18next' {
-  export interface Resources {
-    translation: typeof import('./config/i18n/en.json');
+  interface CustomTypeOptions {
+    // custom namespace type if you changed it
+    defaultNS: 'en';
+    // custom resources type
+    resources: {
+      fr: typeof fr;
+      en: typeof en;
+    };
   }
 }
