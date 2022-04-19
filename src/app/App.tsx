097c0017f42f800dc7cc5dc55f648b33dc04e46b
pchmn <pchmn.dev@gmail.com>
2022-04-19 19:51:41 +0200

    refactor: change components order in `App`

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,36 @@
 import { withAuth } from '@app/core/auth';
 import { AppLayout } from '@app/core/layout';
-import { Home } from '@app/modules/Home';
+import { Home } from '@app/modules/home';
 import { AnimatedRoute } from '@lib/ui';
-import { ReactLocation, Router } from '@tanstack/react-location';
+import { Outlet, ReactLocation, Router } from '@tanstack/react-location';
 import React from 'react';
 import './App.css';
+import { roomRoutes } from './modules/room';
 
 const location = new ReactLocation();
 
 function App() {
   return (
-    <AppLayout>
-      <Router
-        basepath="/voker"
-        location={location}
-        key={location.current.pathname}
-        routes={[
-          {
-            path: '/',
-            element: (
-              <AnimatedRoute>
-                <Home />
-              </AnimatedRoute>
-            )
-          }
-        ]}
-      ></Router>
-    </AppLayout>
+    <Router
+      basepath="/voker"
+      location={location}
+      key={location.current.pathname}
+      routes={[
+        {
+          path: '/',
+          element: (
+            <AnimatedRoute>
+              <Home />
+            </AnimatedRoute>
+          )
+        },
+        roomRoutes
+      ]}
+    >
+      <AppLayout>
+        <Outlet />
+      </AppLayout>
+    </Router>
   );
 }
 
